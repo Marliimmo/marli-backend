83996d67188cf2fd59971f5ba511dda7afa45a4c
Marliimmo <marli.immobilier@gmail.com>
2025-12-14 13:34:00 +0100

    Update pageImageRoutes.js

--- routes/pageImageRoutes.js
+++ routes/pageImageRoutes.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const multer = require("multer");
-const upload = multer({ dest: "uploads/" }); // stockage temporaire avant S3
+const upload = multer({ storage: multer.memoryStorage() }); // stockage en mémoire avant S3
 
 const pageImageController = require("../controllers/pageImageController");
 
