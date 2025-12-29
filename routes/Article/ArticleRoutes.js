a903f36e3b8df8df9e5e80f8f4c7513315912061
Marliimmo <marli.immobilier@gmail.com>
2025-12-29 17:30:51 +0100

    Update ArticleRoutes.js

--- routes/Article/ArticleRoutes.js
+++ routes/Article/ArticleRoutes.js
@@ -3,11 +3,11 @@ const router = express.Router()
 const articleController = require('../../controllers/Article/ArticleController')
 const uploadController = require('../../controllers/Article/UploadImage')
 const auth = require('../../midlewares/auth/auth')
+const upload = require('../../midlewares/upload') // ✅ AJOUT
 
 router.post('/create', auth, articleController.createArticle)
 router.get('/get-articles', articleController.getArticles)
-router.put('/update/:id', auth, articleController.updateArticle) // ✅ NOUVELLE ROUTE
+router.put('/update/:id', auth, articleController.updateArticle)
 router.delete('/delete/:id', auth, articleController.deleteArticle)
-router.post('/upload-image', auth, uploadController.uploadImage)
-
+router.post('/upload-image', auth, upload.single("image"), uploadController.uploadImage) // ✅ CORRECTION
 module.exports = router
